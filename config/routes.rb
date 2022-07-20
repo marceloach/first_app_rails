8222fe418faca846b737f32b86f854e78f66af80
marceloach <cas21014@byui.edu>
2022-07-20 11:02:39 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'pages/one'
+  get 'pages/two'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "pages#one"
+end
